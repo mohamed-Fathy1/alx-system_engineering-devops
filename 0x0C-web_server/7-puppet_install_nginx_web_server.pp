7e7f20081fab21a36e2263137d84e64f17f0db77
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-09-09 10:34:27 +0300

    Add task 7

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -17,8 +17,9 @@ $nginx_file = "
 server {
     listen 80;
     listen [::]:80 default_server;
-    root   /etc/nginx/html;
+    root   /var/www/html;
     index  index.html index.htm;
+
     location /redirect_me {
         return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
     }
