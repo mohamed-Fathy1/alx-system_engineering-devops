ac61cb0fae903ea3dc3e186754e545cb70e8807f
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-11 10:10:27 +0300

    Add task 2'

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,61 +1,64 @@
 #  install and configure an Nginx server using Puppet instead of Bash.
 
 exec { 'apt-get update':
-    command => '/usr/bin/apt-get update',
-    path    => ['/usr/bin', '/usr/sbin'],
-    unless  => '/usr/bin/test -e /var/lib/apt/periodic/update-success-stamp',
+  command => '/usr/bin/apt-get update',
+  path    => ['/usr/bin', '/usr/sbin'],
+  unless  => '/usr/bin/test -e /var/lib/apt/periodic/update-success-stamp',
 }
 
 package { 'nginx':
-    ensure   => 'installed',
-    provider => 'apt',
+  ensure   => 'installed',
+  provider => 'apt',
 }
 
 exec { '/usr/sbin/ufw allow \'Nginx HTTP\'':
+  path    => '/usr/sbin',
+  unless  => '/usr/sbin/ufw status | grep "Nginx HTTP" | grep "ALLOW"',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
-    ensure  => file,
-    content => 'Hello World!',
+  ensure  => file,
+  content => 'Hello World!',
 }
 
 file { '/var/www/error/error_40x.html':
-    ensure  => file,
-    content => 'Ceci n\'est pas une page',
+  ensure  => file,
+  content => 'Ceci n\'est pas une page',
 }
 
 $hostname = $::hostname
 
 $nginx_config = "
 server {
-    listen 80;
-    listen [::]:80 default_server;
-    root   /var/www/html;
-    index  index.html index.htm;
+  listen 80;
+  listen [::]:80 default_server;
+  root   /var/www/html;
+  index  index.html index.htm;
 
-    location / {
-        add_header X-Served-By ${hostname};
-    }
+  location / {
+    add_header X-Served-By ${hostname};
+  }
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
+  location /redirect_me {
+    return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+  }
 
-    error_page 404 /error_40x.html;
-    location = /error_40x.html {
-        root /var/www/error;
-        internal;
-    }
+  error_page 404 /error_40x.html;
+  location = /error_40x.html {
+    root /var/www/error;
+    internal;
+  }
 }"
 
 file { '/etc/nginx/sites-available/default':
-    ensure  => file,
-    content => $nginx_config,
+  ensure  => file,
+  content => $nginx_config,
 }
 
 service { 'nginx':
-    ensure    => 'running',
-    enable    => true,
-    require   => Package['nginx'],
-    subscribe => File['/etc/nginx/sites-available/default'],
+  ensure    => 'running',
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File['/etc/nginx/sites-available/default'],
 }
