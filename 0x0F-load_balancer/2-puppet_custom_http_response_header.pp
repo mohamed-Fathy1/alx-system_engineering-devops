49266501a704d29dabcca99660c2ccd27ac7a635
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-11 10:32:38 +0300

    Add yask 2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,30 +1,15 @@
-#  install and configure an Nginx server using Puppet instead of Bash.
+# Installs a Nginx server with custome HTTP header
 
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update',
-  path    => ['/usr/bin', '/usr/sbin'],
-  unless  => '/usr/bin/test -e /var/lib/apt/periodic/update-success-stamp',
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
-package { 'nginx':
-  ensure   => 'installed',
-  provider => 'apt',
-}
-
-exec { '/usr/sbin/ufw allow \'Nginx HTTP\'':
-  path    => '/usr/sbin',
-  unless  => '/usr/sbin/ufw status | grep "Nginx HTTP" | grep "ALLOW"',
-  require => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => 'Hello World!',
-}
-
-file { '/var/www/error/error_40x.html':
-  ensure  => file,
-  content => 'Ceci n\'est pas une page',
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
 exec { 'add_header':
@@ -34,9 +19,7 @@ exec { 'add_header':
   before      => Exec['restart Nginx'],
 }
 
-service { 'nginx':
-  ensure    => 'running',
-  enable    => true,
-  require   => Package['nginx'],
-  subscribe => File['/etc/nginx/sites-available/default'],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
