4490790c45d22f70d17b84fd789141b521159c0d
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-28 21:55:52 +0200

    Add task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Gather data from an API"""
+"""
+using this REST API, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 import requests
 from sys import argv
 
