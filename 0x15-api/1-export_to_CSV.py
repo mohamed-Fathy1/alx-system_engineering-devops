e5f66974e9e95874d493547542ca66488882b5bb
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-10-28 23:03:57 +0200

    Add task 1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Returns to-do list information for a given employee ID."""
+
+
+if __name__ == '__main__':
+    import requests
+    import csv
+    from sys import argv
+
+    employee_ID = argv[1]
+    req = requests.get("https://jsonplaceholder.typicode.com/todos",
+                       params={"userId": employee_ID})
+
+    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(employee_ID))
+
+    employee = employee.json()
+
+    todos = req.json()
+    completed_todos = []
+
+    for todo in todos:
+        if todo["completed"]:
+            completed_todos.append(todo["title"])
+
+    # export to CSV
+    with open("{}.csv".format(employee_ID), "w") as csvfile:
+        write = csv.writer(csvfile, delimiter=',',
+                           quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        for todo in todos:
+            write.writerow([employee_ID, employee.get("name"),
+                            todo["completed"], todo["title"]])
