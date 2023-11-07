8eafab5bfb0db22b65e70644552336cf9383931e
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-07 20:38:02 +0200

    Add task 100

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -24,13 +24,13 @@ rv:60.0) Gecko/20100101 Firefox/81.0"
     count += res.get('dist')
     titles = res.get('children')
     for title in titles:
-         title = (title.get('data').get('title')).lower().split()
-         for word in title:
-             if word in word_list:
-                 if word in word_dict:
-                     word_dict[word] += 1
-                 else:
-                     word_dict[word] = 1
+        title = (title.get('data').get('title')).lower().split()
+        for word in title:
+            if word in word_list:
+                if word in word_dict:
+                    word_dict[word] += 1
+                else:
+                    word_dict[word] = 1
 
     if after:
         top_hot_subreddit(subreddit, word_list, count, after, word_dict)
@@ -42,9 +42,9 @@ def count_words(subreddit, word_list):
     '''Get the top 10 hot posts for a given subreddit'''
     word_list = [word.lower() for word in word_list]
     result = top_hot_subreddit(subreddit, word_list)
-    sorted_dict = dict(sorted(result.items(), key=lambda item: item[1],
-                              reverse=True))
-    for key, val in sorted_dict.items():
+    sorted_dict = sorted(result.items(), key=lambda item: item[1],
+                         reverse=True)
+    for key, val in sorted_dict:
         print(f"{key}: {val}")
 
     return result
