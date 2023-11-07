d3c673a3b8893a4a57f409850a1d5253052e293c
mohamed-Fathy1 <mohamedfathysaied18@gmail.com>
2023-11-07 17:57:37 +0200

    Add task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+'''Gets the number of subscribers for a given subreddit'''
+import requests
+
+
+def number_of_subscribers(subreddit):
+    '''Gets the number of subscribers for a given subreddit'''
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    headers = {
+                "User-Agent": "Mozilla/5.0 (X11; Linux x86_64;\
+rv:60.0) Gecko/20100101 Firefox/81.0"
+            }
+
+    req = requests.get(url, headers=headers)
+
+    return req.json().get('data').get('subscribers')
